cedbc202294de11d2e3df17ea63bc6a550a88f3c
Leonardo Giovanni Scur <me@kroltan.com>
2026-09-03 19:37:19 -0300

    Put domain redirects before toml ones

--- .netlify/index.js
+++ .netlify/index.js
@@ -1,6 +1,6 @@
 export function onPreBuild({ inputs, netlifyConfig }) {
     for (const [origin, destination] of Object.entries(inputs.forwards)) {
-        netlifyConfig.redirects.push({
+        netlifyConfig.redirects.unshift({
             from: `https://${origin}/*`,
             to: `https://${destination}/:splat`,
             force: true,
